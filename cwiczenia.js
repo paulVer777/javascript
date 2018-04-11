545041b91db5e9bc2c0c6d07919754a5903966c2
paulVer777 <pawel.gitara@onet.eu>
2018-04-11 10:15:00 +0200

    iife

---
+++ cwiczenia.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+(function pytanie() {
+
+
+    var decyzja=confirm("Witaj, czy chcesz odpowiedzieć na pytanie?");
+
+    if(decyzja===true)
+    {
+
+       var imie=prompt("Jak masz na imie?");
+
+        alert(imie);
+    }
+    else return
+
+
+})();
+
